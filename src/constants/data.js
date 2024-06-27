a2998c99fafc8e51f380b8bb5198a3f6fdf35354
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-06-27 16:01:15 +0200

    changed the order socials appear in

--- src/constants/data.js
+++ src/constants/data.js
@@ -7,9 +7,9 @@ const Contents = {
         cvLink: "docs/Antony_Maposa_-_Front_End_Web_Developer.pdf",
         navLinks: ["Home", "About", "Projects", "Contact Me"], 
         socials: [ 
-            {name:"youtube", icon:Images.youtube, link: "https://www.youtube.com/c/Antonycja"}, 
             {name:"github", icon:Images.github, link: "https://github.com/antonycja"}, 
-            {name:"linkedin", icon:Images.linkedin, link: "https://www.linkedin.com/in/antony-maposa/"}
+            {name:"linkedin", icon:Images.linkedin, link: "https://www.linkedin.com/in/antony-maposa/"},
+            {name:"youtube", icon:Images.youtube, link: "https://www.youtube.com/c/Antonycja"}, 
         ]
     }
 };
