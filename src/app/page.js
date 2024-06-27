53c365be3a297342626ff6a8551649459a1a99f7
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-06-27 15:58:01 +0200

    added testing sections on the page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>hello from hero</h1>
-    </div>
+    <>
+      <section id="Home">
+        <h1>hello from hero</h1>
+      </section>
+      <section id="About">
+        <h1>Hello from section 2</h1>
+      </section> 
+    </>
   );
 }
