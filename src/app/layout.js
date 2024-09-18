c7b464191a0ab652d9e73befe3caebad996f4359
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-09-18 16:28:48 +0200

    layout is done with fonts imported

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,59 @@
-import { Inter } from "next/font/google";
+"use client"
+import { Poppins } from "next/font/google";
 import Navbar from "@/components/navbar/navbar"
+import Contents from "@/constants/data.js";
 import "../styles/global.css"
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Antony M | Web Dev",
-  description: "My portfolio website",
-  
-};
+const poppins = Poppins({
+  weight: ['300', '400', '500', '700', '900'],
+  subsets: ["latin"]
+});
+import { useState, useEffect, useRef } from 'react';
 
 export default function RootLayout({ children }) {
+  const [active, setActive] = useState("Home");
+  const sectionsRef = useRef([]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.screenY;
+
+      sectionsRef.current.forEach((section) => {
+        const { top, height, id } = section.getBoundingClientReact();
+
+        if (scrollPosition >= top && scrollPosition < top + height) {
+          setActive(id);
+        }
+      })
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, []);
+
+  const sections = [
+    { id: 'Home', title: 'Home' },
+    { id: 'About', title: 'About' },
+    { id: 'Projects', title: 'Projects' },
+    { id: 'Contact Me', title: 'Contact Me' }
+    // Add more sections as needed
+  ];
+
   return (
     <html lang="en">
-    <head>
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
-    </head>
-    
-      <body className={inter.className}>
-        <Navbar />
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Antony M | Web Dev</title>
+        <meta name="description" content="My portfolio website" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet"/>
+      </head>
+
+      <body className={poppins.className}>
+        <Navbar Contents={Contents} active={active} sections={sections} />
         <main className="main-content">{children}</main>
       </body>
     </html>
