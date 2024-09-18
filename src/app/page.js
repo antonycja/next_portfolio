a1ef1c2ce14ca1ddfd4ae7f092cb24ca702ea8cf
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-09-18 16:31:22 +0200

    added hero page, still need to style the buttons

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,18 @@
+import HeroSection from "@/sections/heroSection/heroPage.js";
+import Contents from "@/constants/data.js";
+
 
 export default function Home() {
   return (
     <>
       <section id="Home">
-        <h1>hello from hero</h1>
+        <HeroSection heroData= {Contents.hero}/>
       </section>
+
+
       <section id="About">
         <h1>Hello from section 2</h1>
-      </section> 
+      </section>
     </>
   );
 }
