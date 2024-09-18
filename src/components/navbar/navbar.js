201173612e10aab4f74ffa3ddc77ededbbe2a191
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-09-18 16:41:50 +0200

    added data file to store web data

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,43 +1,47 @@
-import './navbar.css';
-import Image from "next/image"
-import Contents from "@/constants/data.js"
 
-export default function Navbar() {
+import './navbar.css';
+import Image from "next/image";
+
+export default function Navbar({ Contents, active, sections }) {
+
     return (
         <nav className="nav-container">
-                <div className = "nav-logo">
-                    <Image 
-                        src={Contents.nav.logo} 
-                        width={100}
-                        height={100}
-                        alt="logo" 
-                        priority
-                    />
-                </div>
+            <div className="nav-logo">
+                <Image
+                    src={Contents.nav.logo}
+                    width={100}
+                    height={100}
+                    alt="logo"
+                    priority
+                />
+            </div> 
 
-                <ul className = "nav-links">
-                    {Contents.nav.navLinks.map((navLink, index) => (
-                        <a className="nav-link" href={"#"+navLink} key={index}>
-                            <li className="link">{navLink}</li>
-                        </a>
-                    ))}
-                    <a className="nav-link download-btn" href={Contents.nav.cvLink} download>
-                        <li className="btn">{Contents.nav.btnText}</li>
+            <ul className="nav-links">
+                {sections.map((section, index) => (
+                    <a className={`nav-link ${active === section.id ? 'active' : ''}`}
+                        href={"#" + section.id}
+                        key={index}
+                    >
+                        <li className="link">{section.title}</li>
                     </a>
-                </ul>
+                ))}
+                <a className="nav-link download-btn" href={Contents.nav.cvLink} download>
+                    <li className="btn">{Contents.nav.btnText}</li>
+                </a>
+            </ul>
 
-                <ul className = "social-links">
-                    {Contents.nav.socials.map((social, index) => (
-                        <li className="social-icon" key={index}>
-                            <a href={social.link} target="_blank" >
-                                <Image 
-                                    src= {social.icon}
-                                    alt={social.name}
-                                />
-                            </a>
-                        </li>
-                    ))}
-                </ul>
+            <ul className="social-links">
+                {Contents.nav.socials.map((social, index) => (
+                    <li className="social-icon" key={index}>
+                        <a href={social.link} target="_blank" >
+                            <Image
+                                src={social.icon}
+                                alt={social.name}
+                            />
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </nav>
     )
 }
--- src/constants/data.js
+++ src/constants/data.js
@@ -5,13 +5,18 @@ const Contents = {
         logo: Images.logo,
         btnText: "Download CV",
         cvLink: "docs/Antony_Maposa_-_Front_End_Web_Developer.pdf",
-        navLinks: ["Home", "About", "Projects", "Contact Me"], 
-        socials: [ 
-            {name:"github", icon:Images.github, link: "https://github.com/antonycja"}, 
-            {name:"linkedin", icon:Images.linkedin, link: "https://www.linkedin.com/in/antony-maposa/"},
-            {name:"youtube", icon:Images.youtube, link: "https://www.youtube.com/c/Antonycja"}, 
+        navLinks: ["Home", "About", "Projects", "Contact Me"],
+        socials: [
+            { name: "github", icon: Images.github, link: "https://github.com/antonycja" },
+            { name: "linkedin", icon: Images.linkedin, link: "https://www.linkedin.com/in/antony-maposa/" },
+            { name: "youtube", icon: Images.youtube, link: "https://www.youtube.com/c/Antonycja" },
         ]
+    },
+    hero: {
+        name: { top: "whoami", heading: "Antony Maposa", buttonText: "more about me" },
+        role: { top: "echo $ROLE", heading: "software developer", buttonText: "my projects" }
     }
+    // sections: { home: {}, about: {}, projects: {}, contact: {} }
 };
 
 export default Contents;
