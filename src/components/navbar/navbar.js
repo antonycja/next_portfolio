f2108dda3cc471082c1c581e95075094d052932d
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-09-29 18:59:24 +0200

    updated navbar component

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,7 +6,7 @@ export default function Navbar({ Contents, active, sections }) {
 
     return (
         <nav className="nav-container">
-            <div className="nav-logo">
+            <div className="nav-logo link">
                 <Image
                     src={Contents.nav.logo}
                     width={100}
@@ -26,14 +26,14 @@ export default function Navbar({ Contents, active, sections }) {
                     </a>
                 ))}
                 <a className="nav-link download-btn" href={Contents.nav.cvLink} download>
-                    <li className="btn">{Contents.nav.btnText}</li>
+                    <li className="btn link">{Contents.nav.btnText}</li>
                 </a>
             </ul>
 
             <ul className="social-links">
                 {Contents.nav.socials.map((social, index) => (
                     <li className="social-icon" key={index}>
-                        <a href={social.link} target="_blank" >
+                        <a href={social.link} target="_blank" className='link' >
                             <Image
                                 src={social.icon}
                                 alt={social.name}
