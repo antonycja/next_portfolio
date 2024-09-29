85eb482a7d2ebf256ea371f705ce303276c194e3
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-09-29 19:01:51 +0200

    updated data file with relevant information

--- src/constants/data.js
+++ src/constants/data.js
@@ -15,7 +15,19 @@ const Contents = {
     hero: {
         name: { top: "whoami", heading: "Antony Maposa", buttonText: "more about me" },
         role: { top: "echo $ROLE", heading: "software developer", buttonText: "my projects" }
-    }
+    },
+    about: {
+        title_img: Images.card_title,
+        whoami: { title: "whoami", text: "This is dummy text. The text inside the card. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus eos tempore sed, aliquid eius fugit aspernatur! Enim amet optio exercitationem temporibus mollitia numquam soluta expedita delectus aspernatur. Similique, minus culpa!" },
+
+        skills: {
+            title: "skills",
+            skills: { python: "python", javascript: "javascript", java: "java", react: "reactjs", html5: "html5", css3: "css3", nodedotjs: "node.js", express: "expressjs", nextdotjs: "next.js", sqlite: "sqlite", tailwindcss: "tailwindcss", mongodb: "mongodb", testinglibrary: "Testing", docker: "docker", git: "git", github: "github", gitlab: "gitlab", visualstudiocode: "vscode", figma: "figma", }
+        }
+    },
+    projects: []
+
+    
     // sections: { home: {}, about: {}, projects: {}, contact: {} }
 };
 
