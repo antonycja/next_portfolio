c82146d59b747f25c0b3863f8c2e0b81097efd03
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-10-14 12:17:50 +0200

    added card.js for project view

---
+++ src/components/card/card.js
@@ -0,0 +1,23 @@
+import "./card.css"
+import Image from "next/image"
+
+export default function Card({ onClick, projectData }) {
+
+    return <>
+        <div className="project-card" onClick={onClick}>
+            <div className="card-image">
+                <Image src={projectData.project_images[0].img} alt={projectData.project_images[0].alt} />
+            </div>
+            <div className="card-text">
+                {projectData.project_tags.map((tag, index) => (
+                    <span key={index} className="tag">{tag}</span>
+                ))}
+
+                <h3><span className="project-name">{projectData.project_name}</span> is a {projectData.project_text}</h3>
+                <div className="arrow">
+                    <i className="fas fa-arrow-right card-icon"></i>
+                </div>
+            </div>
+        </div>
+    </>
+};
