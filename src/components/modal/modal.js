a00434dfb513c160d340d665c10e1eb55ddc9f4c
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-10-14 12:26:13 +0200

    added the modal body

---
+++ src/components/modal/modal.js
@@ -0,0 +1,62 @@
+
+import "./modal.css"
+import Carousel from "../Carousel/carousel";
+import ModalNav from "./modalNav";
+import DetailCard from "../card/detailCard";
+
+export default function Modal({ setState, projectsData }) {
+    // const images = [Images.geri, Images.fottis]
+    // const images = Object.keys(Images).map(key => Images[key]);
+
+
+    return (
+        <div className="modal-container" onClick={() => setState(false)}>
+            <div className="modal" onClick={(e) => e.stopPropagation()}>
+                {/* Your scrollable content goes here */}
+                <ModalNav data={projectsData} setState={setState} />
+                <div className="modal-content">
+                    <Carousel images={projectsData.project_images} />
+                    <div className="modal-text">
+                        <div className="modal-summary-section">
+                            <div className="modal-summary-header">
+                                <h1>{projectsData.more.summary.title}</h1>
+                            </div>
+
+                            <article className="modal-summary">
+                                <p>{projectsData.more.summary.text}</p>
+                            </article>
+                        </div>
+
+                        <div className="key-features-section">
+                            <div className="key-feature-header">
+                                <h1>{projectsData.more.all_features.title}</h1>
+                            </div>
+                            <div className="text-block">
+                                <article className="key-feature">
+                                    {projectsData.more.all_features.features.map((currFeature, index) => (
+                                        <DetailCard cardInfo={currFeature} id={index}/>
+                                    ))}
+                                </article>
+                            </div>
+                        </div>
+
+                        <div className="technologies-section">
+                            <div className="technology-header">
+                                <h1>{projectsData.more.all_technologies.title}</h1>
+                            </div>
+
+                            <article className="technologies">
+                                {projectsData.more.all_technologies.technologies.map((currTechnologies, index) => (
+                                    <DetailCard cardInfo={currTechnologies} id={index} />
+                                ))}
+                            </article>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    );
+}
