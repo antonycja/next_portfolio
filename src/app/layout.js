ef46e890aa007f186e1ee0c98429bc1f2c9591cb
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-10-14 12:36:50 +0200

    added new fonts(roboto) and cloudflare to layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -49,7 +49,8 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"></link>
       </head>
 
       <body className={poppins.className}>
