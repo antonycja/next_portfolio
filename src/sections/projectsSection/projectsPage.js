e1bcfa9264cabf30f62fa7be773bcb1cfdfe02b2
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-10-14 13:01:06 +0200

    updated project page

--- src/sections/projectsSection/projectsPage.js
+++ src/sections/projectsSection/projectsPage.js
@@ -1,8 +1,38 @@
-import "./projectsPage.css"
+"use client";
+import { useState, useEffect } from "react";
+import Card from "@/components/card/card";
+import "./projectsPage.css";
+import Modal from "@/components/modal/modal";
 
+export default function ProjectsPage({ projects }) {
+    // Track the index of the currently open card
+    const [selectedProjectIndex, setSelectedProjectIndex] = useState(null);
 
-export default function ProjectsPage() {
-    return <>
+    useEffect(() => {
+        if (selectedProjectIndex !== null) {
+            document.body.style.overflow = "hidden";
+        } else document.body.style.overflow = "scroll";
+        return () => {};
+    }, [selectedProjectIndex]);
 
-    </>
-};
+    const togglePopover = (index) => {
+        // Toggle the modal only for the clicked card
+        setSelectedProjectIndex(index === selectedProjectIndex ? null : index);
+    };
+
+    return (
+        <div className="card-list">
+            {projects.map((project, index) => (
+                <div className="card-item" key={index}>
+                    {selectedProjectIndex === index && (
+                        <Modal
+                            setState={() => setSelectedProjectIndex(null)}
+                            projectsData={project} // Pass the correct project data
+                        />
+                    )}
+                    <Card onClick={() => togglePopover(index)} projectData={project} />
+                </div>
+            ))}
+        </div>
+    );
+}
