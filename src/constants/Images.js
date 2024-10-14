7cac864fb414866c1a38784c43cf8b6d0681bad6
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-10-14 12:57:03 +0200

    added and imported the images into the images file

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -5,6 +5,27 @@ import youtube from "../../public/socials_images/youtube.svg";
 import github from "../../public/socials_images/github.svg";
 import linkedin from "../../public/socials_images/linkedin.svg";
 import card_title from "../../public/cards/card_title.png";
+import geri1 from "../../public/projects/geri1.png";
+import geri2 from "../../public/projects/geri2.png";
+import geri3 from "../../public/projects/geri3.png";
+import geri4 from "../../public/projects/geri4.png";
+import geri5 from "../../public/projects/geri5.png";
+import geri6 from "../../public/projects/geri6.png";
+import fottis1 from "../../public/projects/fottis1.png";
+import fottis2 from "../../public/projects/fottis2.png";
+import fottis3 from "../../public/projects/fottis3.png";
+import fottis4 from "../../public/projects/fottis4.png";
+import fottis5 from "../../public/projects/fottis5.png";
+import fottis6 from "../../public/projects/fottis6.png";
+import camp1 from "../../public/projects/camp1.png";
+import camp2 from "../../public/projects/camp2.png";
+import camp3 from "../../public/projects/camp3.png";
+import camp4 from "../../public/projects/camp4.png";
+import camp5 from "../../public/projects/camp5.png";
+import camp6 from "../../public/projects/camp6.png";
+import lpt1 from "../../public/projects/lpt1.jpg";
+import maze1 from "../../public/projects/maze1.jpg";
+import maze2 from "../../public/projects/maze2.jpg";
 
 
 export const Images = {
@@ -15,4 +36,25 @@ export const Images = {
     github,
     linkedin,
     card_title,
+    geri1,
+    geri2,
+    geri3,
+    geri4,
+    geri5,
+    geri6,
+    fottis1,
+    fottis2,
+    fottis3,
+    fottis4,
+    fottis5,
+    fottis6,
+    camp1,
+    camp2,
+    camp3,
+    camp4,
+    camp5,
+    camp6,
+    lpt1,
+    maze1,
+    maze2,
 };
