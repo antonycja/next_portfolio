cc0b1310738f64141ca118e74e007c38adf2fd11
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-10-14 12:47:51 +0200

    trying to add lazy loading

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,25 @@
-import HeroSection from "@/sections/heroSection/heroPage.js";
-import AboutSection from "@/sections/aboutSection/aboutPage.js";
-import ProjectsSection from "@/sections/projectsSection/projectsPage.js";
+import { lazy, Suspense } from 'react';
 import Contents from "@/constants/data.js";
 
+const HeroSection = lazy(() => import("@/sections/heroSection/heroPage.js"));
+const AboutSection = lazy(() => import("@/sections/aboutSection/aboutPage.js"));
+const ProjectsSection = lazy(() => import("@/sections/projectsSection/projectsPage.js"));
 
 export default function Home() {
   return (
     <>
-      <section id="Home">
-        <HeroSection heroData={Contents.hero} />
-      </section>
-
-
-      <section id="About">
-        <AboutSection aboutData={Contents.about} />
-      </section>
-      <section id="Projects">
-        <ProjectsSection />
-      </section>
+      <Suspense fallback={<div>Good Things take time...<br/>
+      Loading...</div>}>
+        <section id="Home">
+          <HeroSection heroData={Contents.hero} />
+        </section>
+        <section id="About">
+          <AboutSection aboutData={Contents.about} />
+        </section>
+        <section id="Projects">
+          <ProjectsSection projects={Contents.projects} />
+        </section>
+      </Suspense>
     </>
   );
 }
