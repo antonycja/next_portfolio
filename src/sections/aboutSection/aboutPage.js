a4360a25a1bd549ce87de150274672f394b2ce4f
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-11-06 17:05:24 +0200

    fixed card size

--- src/sections/aboutSection/aboutPage.js
+++ src/sections/aboutSection/aboutPage.js
@@ -5,8 +5,10 @@ import AboutCard from '@/components/aboutCard/aboutCard.js'
 export default function aboutPage({ aboutData }) {
     return (<>
         <div className="cards-list">
-            <AboutCard className="who-am-i" data={aboutData.whoami} />
-            <AboutCard className="skills" isIcons={true} data={aboutData.skills} />
+            <div className="cards">
+                <AboutCard className="who-am-i" data={aboutData.whoami} />
+                <AboutCard className="skills" isIcons={true} data={aboutData.skills} />
+            </div>
         </div>
     </>)
 }
