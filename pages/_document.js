ff740c3a77cd257e8db0ff5fcebe9825d4cfc0ea
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-11-08 13:27:07 +0200

    updated lotti and temp contact

--- pages/_document.js
+++ pages/_document.js
@@ -20,25 +20,11 @@ class MyDocument extends Document {
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <meta name="description" content="My portfolio website" />
 
-                    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+
                 </Head>
                 <body>
                     <Main />
                     <NextScript />
-                    {/* JS Deliver (for Bootstrap and Iconify) */}
-                    <script
-                        defer
-                        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-                        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-                        crossOrigin="anonymous"
-                    ></script>
-
-                    <script defer src="https://code.iconify.design/2/2.1.2/iconify.min.js"></script>
-
-                    {/* <script
-                        async
-                        src="https://code.iconify.design/2/2.1.2/iconify.min.js"
-                    ></script> */}
                 </body>
             </Html>
         );
--- src/components/envelope/envelope.js
+++ src/components/envelope/envelope.js
@@ -1,5 +1,19 @@
+'use client'
+import { useEffect } from 'react';
 
 export default function LottieAnimation() {
+    useEffect(() => {
+        // Check if the Lottie Player script is already added
+        if (!document.querySelector("script[src='https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js']")) {
+            const lottieScript = document.createElement("script");
+            lottieScript.src = "https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js";
+            lottieScript.async = true;
+            document.body.appendChild(lottieScript);
+        }
+
+        // No cleanup needed here as the script should stay available for other components
+    }, []);
+
     return (
         <lottie-player
             src="https://assets4.lottiefiles.com/packages/lf20_cbc2dy5y.json"
